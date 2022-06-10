36bcece736f6cd9ad85e02b7b200750fee62bc27
tigerbluejay <josemariairiarte@gmail.com>
2022-06-10 13:37:54 -0300

    EmployeeBL depends con EmployeeDAL

---
+++ DependencyInjectionDemoSolution/DependencyInjectionDemo/EmployeeBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDemo
+{
+    public class EmployeeBL
+    {
+        public EmployeeDAL employeeDAL;
+        public List<Employee> GetAllEmployees()
+        {
+            employeeDAL = new EmployeeDAL();
+            return employeeDAL.SelectAllEmployees();
+        }
+    }
+}
