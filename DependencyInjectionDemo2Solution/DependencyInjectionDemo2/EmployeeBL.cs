9b8041967e05a29f6002e203290afbe8e9237410
tigerbluejay <josemariairiarte@gmail.com>
2022-06-10 14:01:32 -0300

    Dependency Injection through a Property

---
+++ DependencyInjectionDemo2Solution/DependencyInjectionDemo2/EmployeeBL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDemo2
+{
+    public class EmployeeBL
+    {
+        private IEmployeeDAL employeeDAL;
+        public IEmployeeDAL employeeDataObject
+        {
+            set
+            {
+                this.employeeDAL = value;
+            }
+            get
+            {
+                if (employeeDataObject == null)
+                {
+                    throw new Exception("Employee is not initialized");
+                }
+                else
+                {
+                    return employeeDAL;
+                }
+            }
+        }
+        public List<Employee> GetAllEmployees()
+        {
+            return employeeDAL.SelectAllEmployees();
+        }
+    }
+}
---
+++ DependencyInjectionDemo2Solution/DependencyInjectionDemo2/Program.cs
@@ -0,0 +1,22 @@
+﻿using DependencyInjectionDemo2;
+
+namespace DependencyInjectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeBL employeeBL = new EmployeeBL();
+            // injecting the dependency through EmployeeBL property
+            employeeBL.employeeDataObject = new EmployeeDAL();
+
+            List<Employee> ListEmployee = employeeBL.GetAllEmployees();
+            foreach (Employee emp in ListEmployee)
+            {
+                Console.WriteLine("ID = {0}, Name = {1}, Department = {2}",
+                    emp.ID, emp.Name, emp.Department);
+            }
+            Console.ReadKey();
+        }
+    }
+}
