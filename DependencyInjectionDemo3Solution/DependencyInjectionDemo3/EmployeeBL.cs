c1daf6379460aa209d0911eb006ebed5b6e19b9c
tigerbluejay <josemariairiarte@gmail.com>
2022-06-10 14:20:34 -0300

    Dependency Injection through a Method

---
+++ DependencyInjectionDemo3Solution/DependencyInjectionDemo3/EmployeeBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDemo3
+{
+    public class EmployeeBL
+    {
+        public IEmployeeDAL employeeDAL;
+
+        public List<Employee> GetAllEmployees(IEmployeeDAL _employeeDAL)
+        {
+            employeeDAL = _employeeDAL;
+            return employeeDAL.SelectAllEmployees();
+        }
+    }
+}
---
+++ DependencyInjectionDemo3Solution/DependencyInjectionDemo3/Program.cs
@@ -0,0 +1,21 @@
+﻿using DependencyInjectionDemo3;
+
+namespace DependencyInjectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create object of EmployeeBL class
+            EmployeeBL employeeBL = new EmployeeBL();
+            //Call to GetAllEmployees method with proper object.            
+            List<Employee> ListEmployee = employeeBL.GetAllEmployees(new EmployeeDAL());
+            foreach (Employee emp in ListEmployee)
+            {
+                Console.WriteLine("ID = {0}, Name = {1}, Department = {2}", emp.ID,
+                    emp.Name, emp.Department);
+            }
+            Console.ReadKey();
+        }
+    }
+}
